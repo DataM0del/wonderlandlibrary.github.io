b092102b2c649580a1de4e9394c622c4f858d3f6
parkopes <danielthegreat@disroot.org>
2024-06-23 11:29:13 +0200

    All lists now in a single file

--- filler.js
+++ filler.js
@@ -1,62 +1,81 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    const listURL = (() => {
-      switch (document.title) {
-        case "Client Sources":
-          return "https://raw.githubusercontent.com/WonderlandLibrary/client-sources/main/available";
-        case "Client JARs":
-          return "https://raw.githubusercontent.com/WonderlandLibrary/client-jars/main/available";
-        case "Plugin Sources":
-          return "https://raw.githubusercontent.com/WonderlandLibrary/plugin-sources/main/available";
-        case "Plugin JARs":
-          return "https://raw.githubusercontent.com/WonderlandLibrary/plugin-jars/main/available";
-      }
-    })();
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const repo = urlParams.get("repo");
 
-    const folderURL = (() => {
-      switch (document.title) {
-        case "Client Sources":
-          return "https://github.com/WonderlandLibrary/client-sources/tree/main/sources";
-        case "Client JARs":
-          return "https://github.com/WonderlandLibrary/client-jars/tree/main/jars";
-      	case "Plugin Sources":
-          return "https://github.com/WonderlandLibrary/plugin-sources/tree/main/sources";
-        case "Plugin JARs":
-          return "https://github.com/WonderlandLibrary/plugin-jars/tree/main/jars";
-	}
-    })();
+document.addEventListener("DOMContentLoaded", async function () {
+  const listURL = `https://raw.githubusercontent.com/WonderlandLibrary/${repo}/main/available`;
+  const folderURL = `https://github.com/WonderlandLibrary/${repo}/tree/main/sources`;
 
-    // Buttons
-    const response = await fetchWebsiteContent(listURL);
-    const clients = response.split(/\r?\n|\r|\n/g);
-    let output = [];
+  // Buttons
+  const response = await fetchWebsiteContent(listURL);
+  const clients = response.split(/\r?\n|\r|\n/g);
+  let output = [];
 
-    for (let i = 0; i < clients.length; i++) {
-      output[i] = {name: clients[i], url: `${folderURL}/${clients[i]}/`};
+  for (let i = 0; i < clients.length; i++) {
+    output[i] = { name: clients[i], url: `${folderURL}/${clients[i]}/` };
+  }
+
+  header = (() => {
+    switch (repo) {
+      case "client-sources":
+        return {
+          name: "Client Sources",
+          description: "Archived Client sources. Not all are buildable and some may be infected.",
+        };
+
+      case "client-jars":
+        return {
+          name: "Client JARs",
+          description: "Archived Client JARs. Not all are runnable and some may be infected.",
+        };
+
+      case "plugin-sources":
+        return {
+          name: "Plugin Sources",
+          description: "Archived Plugin JARs. Not all are runnable and some may be infected.",
+        };
+
+      case "plugin-jars":
+        return {
+          name: "Plugin JARs",
+          description: "Archived Plugin sources. Not all are buildable and some may be infected.",
+        };
+
+      default:
+        return {
+          name: "¯\\_(ツ)_/¯",
+          description: "Unknown"
+        };
     }
+  })();
 
-    // Get the button grid container
-    const buttonGrid = document.querySelector('.button-grid');
+  const libaryContainer = document.querySelector(".info");
+  libaryContainer.innerHTML =
+    `<h1>${header.name}</h1> <h3>${header.description}</h3>`;
 
-    // Loop through client sources and create buttons
-    output.forEach(source => {
-        const a = document.createElement('a');
-        a.href = source.url;
-        a.className = 'client-button';
-        a.innerHTML = `${source.name}<span class="button-text">Click to view</span>`;
-        buttonGrid.appendChild(a);
-    });
+  // Get the button grid container
+  const buttonGrid = document.querySelector(".button-grid");
+
+  // Loop through client sources and create buttons
+  output.forEach((source) => {
+    const a = document.createElement("a");
+    a.href = source.url;
+    a.className = "client-button";
+    a.innerHTML = `${source.name}<span class="button-text">Click to view</span>`;
+    buttonGrid.appendChild(a);
+  });
 });
 
 async function fetchWebsiteContent(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const text = await response.text();
-        return text;
-    } catch (error) {
-        console.error('Error fetching website content:', error);
-        return "Error";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    const text = await response.text();
+    return text;
+  } catch (error) {
+    console.error("Error fetching website content:", error);
+    return "Error! View console for logs.";
+  }
 }
