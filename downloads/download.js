4aa54190851ca4e9cdfead943036d8d83cfa9102
parkopes <danielthegreat@disroot.org>
2024-07-05 01:01:44 +0200

    Added argon

--- downloads/download.js
+++ downloads/download.js
@@ -2,14 +2,16 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const clientParam = urlParams.get("client");
 
-let clients;
+let data;
 
 document.addEventListener("DOMContentLoaded", async function () {
   await fetch("https://wonderlandlibrary.github.io/featured/data.json")
     .then(response => response.json())
-    .then(json => clients = json.clients);
+    .then(json => data = json);
 
   const container = document.querySelector(".downloads-container");
+
+  let clients = data.clients;
 
   let client;
 
@@ -19,25 +21,36 @@ document.addEventListener("DOMContentLoaded", async function () {
       client = it;
   }
 
-  const clientName = document.createElement("h1");
-  clientName.innerHTML = client.name;
+  if (client.downloadType == "standard") {
+    const clientName = document.createElement("h1");
+    clientName.innerHTML = client.name;
 
-  const description = document.createElement("h3");
-  description.innerHTML = `Download different versions of the ${client.name} client`;
+    const description = document.createElement("h3");
+    description.innerHTML = `Download different versions of the ${client.name} client`;
 
-  container.appendChild(clientName);
-  container.appendChild(description);
+    container.appendChild(clientName);
+    container.appendChild(description);
 
-  const buttonGrid = document.createElement("div");
-  buttonGrid.className = "button-grid";
+    const buttonGrid = document.createElement("div");
+    buttonGrid.className = "button-grid";
 
-  for (const download of client.links) {
-    const button = document.createElement("a");
-    button.href = download.link;
-    button.className = "download-button";
-    button.innerHTML = download.name + "<span class=\"button-text\">Click to download</span>";
-    buttonGrid.appendChild(button);
+    for (const download of client.links) {
+      const button = document.createElement("a");
+      button.href = download.link;
+      button.className = "download-button";
+      button.innerHTML = download.name + "<span class=\"button-text\">Click to download</span>";
+      buttonGrid.appendChild(button);
+    }
+
+    container.appendChild(buttonGrid);
+  } else if (client.downloadType == "coming") {
+    const h1 = document.createElement("h1");
+    h1.innerHTML = "Coming soon";
+    const gif = document.createElement("img");
+    gif.src = "https://media1.tenor.com/m/6be0tZhr50QAAAAC/lazy-cat.gif";
+    gif.alt = "Kitty cat gif";
+
+    container.appendChild(p);
+    container.appendChild(gif);
   }
-
-  container.appendChild(buttonGrid);
 });
